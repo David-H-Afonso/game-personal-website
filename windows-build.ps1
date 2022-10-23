ac466470bfa358d7e0148fcc19ba47e4fecafec6
David Hormiga Afonso <davidhormigafonso@gmail.com>
2022-10-23 23:49:16 +0100

    deploy

--- windows-build.ps1
+++ windows-build.ps1
@@ -5,17 +5,17 @@ yarn build
 git add .
 git commit -m "deploy"
 git push origin develop
-cd ".."
-cd "D:\Programacion\personal-website\deploy-game-personal-website"
+Set-Location ".."
+Set-Location "deploy-game-personal-website\"
 git pull origin master
 git stash
 git rm -r *
 git fetch --all
 git checkout origin/develop -- build/
-git mv (gci build/*) .
+git mv (Get-ChildItem build/*) .
 git add .
 git commit -m "deploy"
 git push origin master
-cd ".."
-cd "D:\Programacion\personal-website\game-personal-website"
+Set-Location ".."
+Set-Location "game-personal-website\"
 git checkout develop
